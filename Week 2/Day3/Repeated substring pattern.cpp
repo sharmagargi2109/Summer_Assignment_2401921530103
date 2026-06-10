cd60c0d796ad9d25a1e9e12476b4c8d5bb729232
sharmagargi2109 <sharmagargi2109@gmail.com>
2026-06-10 20:53:38 +0530

    Create Repeated substring pattern.cpp
    Repeated substring pattern

---
+++ Week 2/Day3/Repeated substring pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+      for (int l = 1; l<=n/2; l++) {
+                 if (n % l== 0) {
+                 string sub = s.substr(0, l);
+                string temp = "";
+
+                for (int i=0; i<n/l; i++) {
+                    temp += sub;
+                }
+                 if (temp == s) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
