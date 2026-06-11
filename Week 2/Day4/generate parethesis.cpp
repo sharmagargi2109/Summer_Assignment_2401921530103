7986c43e555d2a8f185b85b0c137eef63a632d74
sharmagargi2109 <sharmagargi2109@gmail.com>
2026-06-11 20:47:19 +0530

    Create generate parethesis.cpp
    generate Parenthesis

---
+++ Week 2/Day4/generate parethesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> ans;
+     void generate(string s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+     if (open < n)
+            generate(s + "(", open + 1, close, n);
+
+        if (close < open)
+            generate(s + ")", open, close + 1, n);
+    }
+    vector<string> generateParenthesis(int n) {
+        generate("", 0, 0, n);
+        return ans;
+    }
+};
